be157151051128c19a9622bd8888ab8a5a6d5114
Sahil Lamba <sahil.lamba95@gmail.com>
2019-06-29 22:46:11 +0530

    Add firebase, env config, routing logic in App

--- pingmyhuskyapp/App.js
+++ pingmyhuskyapp/App.js
@@ -3,13 +3,51 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
+import Config from 'react-native-config'
+import firebase from 'firebase'
+
 import MyWebView from './src/webview';
 
+const fireConfig = {
+  apiKey: Config.apiKey,
+  authDomain: Config.authDomain,
+  databaseURL: Config.databaseURL,
+  projectId: Config.projectId,
+  storageBucket: Config.storageBucket,
+  messagingSenderId: Config.messagingSenderId,
+  appId: Config.appId,
+}
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(fireConfig);
+}
+
 export default class App extends Component {
+  state = {
+    uri: ""
+  }
+
+  componentWillMount() {
+    firebase.database().ref('stream').on('value', snapshot => {
+      const stream = snapshot.val()
+      const httpsTunnel = stream.tunnels && stream.tunnels.length ? stream.tunnels.filter(t => t.proto === 'https')[0] : {
+        public_url: "https://www.google.com"
+      }
+      this.setState({
+        uri: httpsTunnel.public_url
+      })
+    })
+  }
+
   render() {
-    return (
-      <MyWebView/>
+    return ( <
+      MyWebView uri = {
+        this.state.uri
+      }
+      />
     );
   }
 }
--- pingmyhuskyapp/src/webview.js
+++ pingmyhuskyapp/src/webview.js
@@ -1,13 +1,18 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { View, StyleSheet } from 'react-native'
 import { WebView } from 'react-native-webview'
 
-const MyWebView = () => {
+const MyWebView = props => {
   return (
     <View style={styles.container}>
-      <WebView source={{ uri: 'https://www.google.com' }}/>
+      <WebView source={{ uri: props.uri }}/>
     </View>
    )
+}
+
+MyWebView.propTypes = {
+  uri: PropTypes.string.isRequired
 }
 
 export default MyWebView;
